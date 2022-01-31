406ddd79b6580c341f0cbb388d8e7603cd8417af
nouman <noumanuskt002@gmail.com>
2022-01-31 12:33:12 +0500

    jquery

---
+++ Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using ChainStoreSystem.Data;
+using ChainStoreSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChainStoreSystem.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ChainStoreDbContext _context;
+
+        public CartController(ChainStoreDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult AddToCart(Product CartProduct)
+        {
+
+            return View();
+        }
+    }
+}
